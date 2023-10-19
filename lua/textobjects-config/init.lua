b8a828b6fe10ebcf306f3dc3319d0edf7ca20a11
TA-800 <taherali0905@gmail.com>
2023-10-18 23:08:42 -0400

    more textobjs, c = conditional, L = loop...

--- lua/textobjects-config/init.lua
+++ lua/textobjects-config/init.lua
@@ -3,7 +3,14 @@ local spec_treesitter = require('mini.ai').gen_spec.treesitter
 require('mini.ai').setup({
   custom_textobjects = {
     f = spec_treesitter({
-      a = '@function.outer', i = '@function.inner',
+      a = "@function.outer", i = "@function.inner",
+    }),
+    -- don't use l because mini ai uses that for "last", for example: "v i l '" to select last inner '' content
+    L = spec_treesitter({
+      a = "@loop.outer", i = "@loop.inner",
+    }),
+    c = spec_treesitter({
+      a = "@conditional.outer", i = "@conditional.inner",
     }),
   }
 })
